ed7e23074d157bd1c311be70714c626a463296d4
José Alves <metrodf2013@gmail.com>
2019-06-11 15:28:06 -0300

    3º inserir um áudio b7 sobre estourar balões

--- js/script.js
+++ js/script.js
@@ -1,22 +1,37 @@
 function addBola() {
 	var bola = document.createElement("div");
-	bola.setAttribute("class","bola");
-	// == <div class="bola"></div>
+	bola.setAttribute("class", "bola");
 
 	var p1 = Math.floor(Math.random() * 500);
-	var p2 = Math.floor(Math.random() * 400);	
+	var p2 = Math.floor(Math.random() * 400);
 	bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;");
-	//<div style="left:0129;top:12903"></div>
-	bola.setAttribute("onclick", "estourar(this)");  
+	bola.setAttribute("onclick", "estourar(this)");
 
 	document.body.appendChild(bola);
-	//inserindo o elemento na tela.
 }
+
 function estourar(elemento) {
 	document.body.removeChild(elemento);
-//aqui está removendo o elemento da tela.
-
 }
+
 function iniciar() {
 	setInterval(addBola, 1000);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
